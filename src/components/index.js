d876991b7e1ebd7f4199c5dea671eacbf9a2f200
Ali Zahid <ali.zahid@live.com>
2019-01-07 04:58:07 +0400

    Refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Head from './head'
 import Header from './header'
-import Post from './post'
 import Sidebar from './sidebar'
 import Spinner from './spinner'
 
-export { Head, Header, Post, Sidebar, Spinner }
+export { Head, Header, Sidebar, Spinner }
--- src/components/post/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-import './index.scss'
-
-export default class Post extends Component {
-  render() {
-    return <div>Sidebar</div>
-  }
-}
