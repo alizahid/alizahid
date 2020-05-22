70c9862f1d50835df6d5c9c590a870ddcbbbb8a2
Ali Zahid <ali.zahid@live.com>
2020-05-23 02:42:01 +0400

    Bug fix

--- prismic.d.ts
+++ prismic.d.ts
@@ -1,7 +1,1 @@
-import { FunctionComponent } from 'react'
-
-declare module 'prismic-reactjs' {
-  export const RichText: FunctionComponent<{
-    render: []
-  }>
-}
+declare module 'prismic-reactjs'
