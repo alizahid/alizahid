c0fd92f8ecb470cc670f74508741fe67d35a9d34
Ali Zahid <ali.zahid@live.com>
2020-10-12 21:31:14 +0500

    OG meta added

--- components/page.tsx
+++ components/page.tsx
@@ -23,6 +23,7 @@ export const Page: FunctionComponent<Props> = ({
       <title>{[subtitle, title].filter(Boolean).join(' / ')} / Ali Zahid</title>
       <meta content={description} name="description" />
       <meta content={description} property="og:description" />
+      <meta content="website" property="og:type" />
     </Head>
 
     <main className="max-w-5xl min-h-screen flex flex-col justify-center p-8 lg:p-20">
