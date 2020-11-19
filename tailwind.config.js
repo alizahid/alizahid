48c35f9721ced3b43f552eb6056fd4d9985e0c25
Ali Zahid <ali.zahid@live.com>
2020-11-19 11:34:00 +0500

    Dependencies upgraded

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  future: {
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true
-  },
   plugins: [],
   purge: ['./**/*.tsx', './assets/global.scss'],
   theme: {
+    colors: {
+      black: '#000',
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c'
+      },
+      white: '#fff'
+    },
     extend: {
       colors: {
         highlight: 'rgba(0, 0, 0, 0.5)',
