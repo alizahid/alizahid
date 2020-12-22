307098a7e373cae9756e150bf583600633960578
Ali Zahid <ali.zahid@live.com>
2020-12-22 14:19:18 +0400

    Post: Going ham with Next.js

--- components/image.tsx
+++ components/image.tsx
@@ -1,19 +1,34 @@
+import NextImage from 'next/image'
 import React, { FunctionComponent } from 'react'
 
 type Props = {
   caption: string
   className?: string
+  height?: number
   image: string
+  width?: number
 }
 
 export const Image: FunctionComponent<Props> = ({
   caption,
   className,
-  image
+  height,
+  image,
+  width
 }) => (
   <figure className={`my-8 ${className}`}>
-    <img alt={caption} className="block mx-auto rounded-lg" src={image} />
-    <figcaption className="text-gray-500 text-center mt-4 mx-4">
+    {height && width ? (
+      <NextImage
+        alt={caption}
+        className="block mx-auto rounded-lg"
+        height={height}
+        src={image}
+        width={width}
+      />
+    ) : (
+      <img alt={caption} className="block mx-auto rounded-lg" src={image} />
+    )}
+    <figcaption className="text-gray-500 text-center text-sm mt-2 mx-4">
       {caption}
     </figcaption>
   </figure>
