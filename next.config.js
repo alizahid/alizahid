0037e5d1df19893184a28c29e6c4c404bc6b1113
Ali Zahid <ali.zahid@live.com>
2021-04-13 09:17:54 +0500

    Redirect Index to Willa

--- next.config.js
+++ next.config.js
@@ -4,5 +4,14 @@ module.exports = {
   },
   images: {
     domains: ['media.graphcms.com']
+  },
+  async redirects() {
+    return [
+      {
+        destination: 'https://willa.app?utm_source=index',
+        permanent: true,
+        source: '/index'
+      }
+    ]
   }
 }
