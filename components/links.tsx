0d63983d1a320428fdf65f35f1790de12578187f
Ali Zahid <ali.zahid@live.com>
2021-09-03 08:51:13 +0500

    Styles updated

--- components/links.tsx
+++ components/links.tsx
@@ -42,7 +42,7 @@ export const LinkCard: FunctionComponent<LinkProps> = ({ className, link }) => (
       rel="noopener"
       target="_blank">
       <div
-        className="w-12 h-12 bg-gray-200 bg-center bg-cover rounded-full dark:bg-gray-800"
+        className="w-12 h-12 bg-center bg-cover rounded-full"
         style={{
           backgroundImage: `url(${link.image})`
         }}
