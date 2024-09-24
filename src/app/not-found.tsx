cec1e178cfa2dcc7edd833b005742fab7eea0221
Ali Zahid <ali.zahid@live.com>
2024-09-24 09:54:31 +0500

    Styles updated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,6 @@
+import { House } from '@phosphor-icons/react/dist/ssr'
 import { type Metadata } from 'next'
+import Link from 'next/link'
 import { ThemeProvider } from 'next-themes'
 
 export const metadata: Metadata = {
@@ -8,7 +10,11 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <ThemeProvider attribute="class">
-      <main className="flex min-h-screen flex-col justify-center gap-6 text-pretty p-6">
+      <main className="flex min-h-screen max-w-4xl flex-col items-start justify-center gap-6 text-pretty p-6 lg:p-8">
+        <Link href="/">
+          <House className="size-6" />
+        </Link>
+
         <h2 className="text-9 font-bold">
           Looks like you&#39;re looking for something that doesn&#39;t exist.
         </h2>
